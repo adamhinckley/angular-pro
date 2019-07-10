2ef5a81584cda16fc8363158f02dedbcd5810a49
Adam Hinckley <adamhinckley@mac.com>
2019-07-09 22:07:46 -0500

    Using ng-content with projection slots

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,18 +1,18 @@
 import { Component } from "@angular/core";
 
 import { User } from "./auth-form/auth-form.interface";
-// the h3 tags between the auth-form tags are projected to
-// the child.  When the element is inspected in the
-// browser, it shows the h3 tags inside the form.
+
 @Component({
     selector: "app-root",
     template: `
         <div>
             <auth-form (submitted)="createUser($event)">
                 <h3>Create Account</h3>
+                <button type="submit">Join Us</button>
             </auth-form>
             <auth-form (submitted)="loginUser($event)">
                 <h3>Login</h3>
+                <button type="submit">Login</button>
             </auth-form>
         </div>
     `
--- app/auth-form/auth-form.component.ts
+++ app/auth-form/auth-form.component.ts
@@ -2,14 +2,12 @@ import { Component, Output, EventEmitter } from "@angular/core";
 
 import { User } from "./auth-form.interface";
 
-// the ng-content tag is necessary for grapping and displaying the
-// the h3's in app.component.ts (parent)
 @Component({
     selector: "auth-form",
     template: `
         <div>
             <form (ngSubmit)="onSubmit(form.value)" #form="ngForm">
-                <ng-content></ng-content>
+                <ng-content select="h3"></ng-content>
                 <label>
                     Email address
                     <input type="email" name="email" ngModel />
@@ -18,9 +16,7 @@ import { User } from "./auth-form.interface";
                     Password
                     <input type="password" name="password" ngModel />
                 </label>
-                <button type="submit">
-                    Submit
-                </button>
+                <ng-content select="button"></ng-content>
             </form>
         </div>
     `
