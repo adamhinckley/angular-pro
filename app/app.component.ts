8f4d7be9fe01222306bda32baa579e58c2f3933b
Adam Hinckley <adamhinckley@mac.com>
2019-07-09 21:56:34 -0500

    Content projection with ng-content

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,30 +1,28 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { User } from './auth-form/auth-form.interface';
-
+import { User } from "./auth-form/auth-form.interface";
+// the h3 tags between the auth-form tags are projected to
+// the child.  When the element is inspected in the
+// browser, it shows the h3 tags inside the form.
 @Component({
-  selector: 'app-root',
-  template: `
-    <div>
-      <auth-form 
-        (submitted)="createUser($event)">
-        <h3>Create account</h3>
-      </auth-form>
-      <auth-form 
-        (submitted)="loginUser($event)">
-        <h3>Login</h3>
-      </auth-form>
-    </div>
-  `
+    selector: "app-root",
+    template: `
+        <div>
+            <auth-form (submitted)="createUser($event)">
+                <h3>Create Account</h3>
+            </auth-form>
+            <auth-form (submitted)="loginUser($event)">
+                <h3>Login</h3>
+            </auth-form>
+        </div>
+    `
 })
 export class AppComponent {
+    createUser(user: User) {
+        console.log("Create account", user);
+    }
 
-  createUser(user: User) {
-    console.log('Create account', user);
-  }
-
-  loginUser(user: User) {
-    console.log('Login', user);
-  }
-
+    loginUser(user: User) {
+        console.log("Login", user);
+    }
 }
--- app/auth-form/auth-form.component.ts
+++ app/auth-form/auth-form.component.ts
@@ -1,34 +1,34 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter } from "@angular/core";
 
-import { User } from './auth-form.interface';
+import { User } from "./auth-form.interface";
 
+// the ng-content tag is necessary for grapping and displaying the
+// the h3's in app.component.ts (parent)
 @Component({
-  selector: 'auth-form',
-  template: `
-    <div>
-      <form (ngSubmit)="onSubmit(form.value)" #form="ngForm">
-        <ng-content></ng-content>
-        <label>
-          Email address
-          <input type="email" name="email" ngModel>
-        </label>
-        <label>
-          Password
-          <input type="password" name="password" ngModel>
-        </label>
-        <button type="submit">
-          Submit
-        </button>
-      </form>
-    </div>
-  `
+    selector: "auth-form",
+    template: `
+        <div>
+            <form (ngSubmit)="onSubmit(form.value)" #form="ngForm">
+                <ng-content></ng-content>
+                <label>
+                    Email address
+                    <input type="email" name="email" ngModel />
+                </label>
+                <label>
+                    Password
+                    <input type="password" name="password" ngModel />
+                </label>
+                <button type="submit">
+                    Submit
+                </button>
+            </form>
+        </div>
+    `
 })
 export class AuthFormComponent {
+    @Output() submitted: EventEmitter<User> = new EventEmitter<User>();
 
-  @Output() submitted: EventEmitter<User> = new EventEmitter<User>();
-
-  onSubmit(value: User) {
-    this.submitted.emit(value);
-  }
-
+    onSubmit(value: User) {
+        this.submitted.emit(value);
+    }
 }
