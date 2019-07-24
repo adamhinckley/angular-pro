4d98bfddd9f223ee82a771e45d1f5a44e44c497b
Adam Hinckley <adamhinckley@mac.com>
2019-07-23 23:44:22 -0600

    creates a custom pipe

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
-import { MyForDirective } from "./my-for/my-for.directive";
+
+import { FileSizePipe } from "./filesize.pipe";
 
 @NgModule({
-    declarations: [AppComponent, MyForDirective],
+    declarations: [AppComponent, FileSizePipe],
     imports: [BrowserModule],
     bootstrap: [AppComponent]
 })
