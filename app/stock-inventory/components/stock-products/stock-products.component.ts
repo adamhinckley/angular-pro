c13fa4353b5400af7d25d41b96c1133ce3c6c9af
Adam Hinckley <adamhinckley@mac.com>
2019-07-30 22:32:11 -0600

    Dynamically adds price and product name and utilizes the currency pipe

--- app/stock-inventory/components/stock-products/stock-products.component.ts
+++ app/stock-inventory/components/stock-products/stock-products.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, Output, EventEmitter } from "@angular/core";
 import { FormGroup, FormArray } from "@angular/forms";
+import { Product } from "../../models/product.interface";
 
 @Component({
     selector: "stock-products",
@@ -10,8 +11,15 @@ export class StockProductsComponent {
     @Input()
     parent: FormGroup;
 
+    @Input()
+    map: Map<number, Product>;
+
     @Output()
     removed = new EventEmitter<any>();
+
+    getProduct(id) {
+        return this.map.get(id);
+    }
 
     onRemove(group, index) {
         this.removed.emit({ group, index });
